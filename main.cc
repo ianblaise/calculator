5ad204a87e33c9b7fa318d99e46f93bf5ad17b94
tucker0410 <tucker.0410@gmail.com>
2017-05-05 13:18:05 -0700

    update 2

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 //super smart code in main from tucker
+//second attempt at super smart code from tucker
 int main () {
 	cout << "this is a calcutalor it will do something";
 	return 0;
