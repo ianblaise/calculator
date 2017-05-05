799eee55d29b83c9134b7fc9438275f948c2b40c
Anelon <majacow@gmail.com>
2017-05-05 13:46:10 -0700

    merge fail

--- main.cc
+++ main.cc
@@ -5,19 +5,6 @@ void tolower(string word) {
 }
 //super smart code in main from tucker
 //second attempt at super smart code from tucker
-<<<<<<< HEAD
-
-
-
-
-
-
-int main () {
-
-
- 
-
-=======
 class Variables {
 	vector<int> letters; //set to only have 26 spaces
 	public:
@@ -30,7 +17,6 @@ int main () {
 	while(cin){
 		cin >> operat;
 	}
->>>>>>> d3975c2281c3c4677605a3160bae8b844b987ad6
 	cout << "this is a calcutalor it will do something";
 	return 0;
 }
