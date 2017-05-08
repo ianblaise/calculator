675f22e801f664f80a73dca6f747cfd3ee62ee10
Anelon <majacow@gmail.com>
2017-05-08 13:56:24 -0700

    added do_math() function

--- main.cc
+++ main.cc
@@ -5,6 +5,28 @@ void tolower(string& word) {
 	for (auto& c: word)
 		c = tolower(c);
 }
+//askii goes up to 255 so it should work all the way with chars?
+//unsigned chars go to 255
+int do_math(string equn) { //can change if you want to use queue
+	int equals = 0;
+	for (unsigned char i: equn) {
+		if (i == ' ') continue;
+		if (isalpha(i)) {
+			if (i == '+') ;//add the next to equals
+			else if (i == '-') ;//subtract the next from equals
+			else if (i == '*') ;//multiply equals by next
+			else if (i == '/') ;//divide equals by next
+			else if (i == '^') ;//equals pow(next)
+			else if (i == '%') ;//equals mod next
+			else {//if vatiable should only run the first time? 
+			}
+		} else {//should only run the first time
+			equals = i;
+		}
+	}
+	return equals;
+}
+
 //super smart code in main from tucker
 //second attempt at super smart code from tucker
 class Variables {
@@ -12,7 +34,9 @@ class Variables {
 		vector<int> letters; //set to (26, 256) 
 	public:
 		Variables() : letters(26,256) {}
-		void set(int num, char let);
+		void set(int num, char let) {
+			letters.at(let - 'a') = num;
+		}
 		int get(char let) {return letters.at(let);}
 };
 int main () {
